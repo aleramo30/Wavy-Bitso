645ba5b648c09906b2e9c517919f879d09e6ad3d
aleramo30 <114834532+aleramo30@users.noreply.github.com>
2025-07-14 11:07:22 -0600

    Update page.tsx

--- wavy/src/app/page.tsx
+++ wavy/src/app/page.tsx
@@ -1,22 +1,9 @@
-'use client'
-
-import Hero from "@/components/landing/hero/Hero";
-import VideoDemoSection from "@/components/landing/video/VideoDemoSection";
-import Footer from "@/components/landing/Footer";
-import Navbar from "@/components/landing/navbar/Navbar";
-import FoundersSection from "@/components/landing/founders/FoundersSection";
-import FaqSection from "@/components/landing/faq/FaqSection";
-import CompaniesSection from "@/components/landing/CompaniesSection";
+import { Search } from "@/components/search/search";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col">
-      <Navbar />
-      <Hero />
-      <VideoDemoSection />  
-      <CompaniesSection/>
-      <FaqSection/>
-      <Footer />
-    </main>
+    <div className="container mx-auto py-6 space-y-8">
+      <Search />
+    </div>
   );
-} 
+}
