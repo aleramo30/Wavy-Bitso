b0e0041e8ac02b1f2cb9aa4241ea9b88ed2932f2
aleramo30 <114834532+aleramo30@users.noreply.github.com>
2025-07-14 11:04:38 -0600

    Delete Pagesrc/app/page.tsx

--- Pagesrc/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Search } from "@/components/search/search";
-
-export default function Home() {
-  return (
-    <div className="container mx-auto py-6 space-y-8">
-      <Search />
-    </div>
-  );
-}
